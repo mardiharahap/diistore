151e69a6532959471182d6a7e3f0063cb8793f79
mardiharahap <134383375+mardiharahap@users.noreply.github.com>
2025-09-28 14:22:44 +0700

    Update transactions.js

--- src/app/api/transactions.js
+++ src/app/api/transactions.js
@@ -1,1 +1,15 @@
+import fs from "fs";
+import path from "path";
 
+export default async function handler(req, res) {
+  const filePath = path.join(process.cwd(), "pages/api/_transactions.json");
+
+  let transactions = [];
+  try {
+    transactions = JSON.parse(fs.readFileSync(filePath, "utf-8"));
+  } catch (err) {
+    transactions = [];
+  }
+
+  res.status(200).json({ ok: true, data: transactions });
+}
